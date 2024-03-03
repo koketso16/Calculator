7450ce758d18d25e7bf0a6c4d42390d0cc94fb27
Koketjo Mothapo <99620457+koketso16@users.noreply.github.com>
2024-03-03 15:34:44 +0200

    working on calculator

--- calculator.py
+++ calculator.py
@@ -1,1 +1,20 @@
+class Calculator:
+    def __init__(self):
+        self.equation = ""
+
+    def add_to_equation(self, value):
+        self.equation += str(value)
+
+    def clear_equation(self):
+        self.equation = ""
+
+    def calculate_result(self):
+        try:
+            result = eval(self.equation)
+            return result
+        except Exception as e:
+            return "Error"
+
+if __name__ == "__main__":
+    calc = Calculator()
 
